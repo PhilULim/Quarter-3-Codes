90e7d023509394088ab25e764404119b2b90a087
PhilULim <philjustin.lim@smc.pshs.edu.ph>
2026-01-15 23:51:04 +0800

    Lim_Q3_FA04.py
    This code gets the total amount of steps done per person and summarizes it.

---
+++ Quarter 3- FA4.py
@@ -0,0 +1,13 @@
+
+people = []
+
+print("This program is meant to be a competition between you and your friends to see which one walks too much. It will ask between 5 week days.")
+peeps = int(input("How many ppl r joining this?"))
+for i in range(peeps):
+    person = input(f"Please input person {1}:")
+    people.append(person)
+print (people)
+people[0]=[]
+queen=("queen")
+people[0].append(queen)
+print(people)
