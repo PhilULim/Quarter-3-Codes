4b98a8adabe167e6b139fc03fb04d2c1a9d81da1
PhilULim <philjustin.lim@smc.pshs.edu.ph>
2026-01-25 20:17:00 +0800

    Lim_Q3_FA06.py
    This asks the user to input their name, age, and favorite subject then uploads the data to a dictionary then prints it out.

---
+++ Quarter3-FA6(actual).py
@@ -0,0 +1,11 @@
+student = {}
+
+input_name = input("Enter your name:")
+input_age = int(input("Enter your age:"))
+input_favsubj = input("Enter your favorite subject:")
+
+student["Name"] = input_name
+student["Age"] = input_age
+student["Favorite Subject"] = input_favsubj
+
+print("Student Record:", "\n",student)
